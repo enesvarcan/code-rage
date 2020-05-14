cb71d0712dfab2e4a462be827d541cb20e1b981b
Varcan Enes <evarcan@unibz.it>
2020-05-14 15:35:21 +0200

    Update errorhandler

--- src/middlewares/errorhandler.mw.js
+++ src/middlewares/errorhandler.mw.js
@@ -2,5 +2,11 @@ exports.errorHandler = (err, req, res, next) => {
     if(err.code === 11000) {
         res.status(406).send({code: 01, message: 'Username is already taken!'})
     }
+
+    if(err.errors){
+        if(err.errors.username || err.errors.email || err.errors.password){
+            res.status(400).send({code: 02, message: 'Fill all required fields!'})
+        }
+    }
 }
 
