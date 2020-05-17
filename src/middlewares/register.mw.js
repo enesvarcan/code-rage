adf7a677e197b02774c147b2fd97005cd187b788
Varcan Enes <evarcan@unibz.it>
2020-05-17 16:45:44 +0200

    Delete comments

--- src/middlewares/register.mw.js
+++ src/middlewares/register.mw.js
@@ -4,14 +4,11 @@ exports.saveUserIntoDB = (req, res, next) => {
 
     var user = new User(req.userInfo)
 
-    //user.userId = user._id
-
     user.save((err, usr) => {
         
         if(err){
             return next(err)
         }
         res.send({code: 00, message: 'Registration Complete!'})
-        //res.send('user registered')
     })
 }
