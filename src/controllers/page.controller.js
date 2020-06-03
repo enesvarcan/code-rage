de86a81a24052df86fc1cbf3288e8e0d2d6d1828
Varcan Enes <evarcan@unibz.it>
2020-06-03 12:50:46 +0200

    Login page

--- src/controllers/page.controller.js
+++ src/controllers/page.controller.js
@@ -3,3 +3,7 @@ const path = require('path')
 exports.registerPage = (req, res, next) => {
     res.sendFile(path.join(__dirname, '..', '..', 'views/register.html'))
 }
+
+exports.loginPage = (req, res, next) => {
+    res.sendFile(path.join(__dirname, '..', '..', 'views/login.html'))
+}
