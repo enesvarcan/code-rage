33c37acc8a4f9d785a37f9a5bdd1d5733006b95c
Enes Varcan <enesvarcan@gmail.com>
2020-06-08 13:35:46 +0200

    Define functions

---
+++ src/services/photo.service.js
@@ -0,0 +1,21 @@
+//require fs?, path?
+
+exports.uploadPhoto = (photo, cb) => {
+
+    if(!photo){
+        //upload default photo
+    }
+
+}
+
+exports.updatePhoto = (photo, cb) => {
+
+}
+
+exports.deletePhoto = (path, cb) => {
+
+}
+
+exports.readPhoto = (path, cb) => {
+    
+}
