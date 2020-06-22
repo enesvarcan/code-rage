4c14a0e14ab224d8d63058edaf0df51a6a5d549d
Enes Varcan <enesvarcan@gmail.com>
2020-06-22 14:36:02 +0200

    refresh

--- src/config/db.connection.js
+++
@@ -1,2 +0,0 @@
-const CONNSTR = "mongodb://localhost:27017/code-rage"
-module.exports = CONNSTR
--- src/helpers/error.handler.js
+++
@@ -1,9 +0,0 @@
-class ErrorHandler extends Error {
-    constructor(status, message){
-        super()
-        this.status = status
-        this.message = message
-    }
-}
-
-module.exports = ErrorHandler
